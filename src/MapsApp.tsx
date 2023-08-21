c51aa082f78f484dd153afeae0b54fa53ce678be
Hugo <hugo@boahstudio.com>
2023-08-21 00:41:34 -0300

    Context Created

---
+++ src/MapsApp.tsx
@@ -0,0 +1,10 @@
+import { PlacesProvider } from "./context"
+
+
+export const MapsApp = () => {
+  return (
+    <PlacesProvider>
+        <h1>Hola mundo de nuevo</h1>
+    </PlacesProvider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App.tsx'
+import { MapsApp } from './MapsApp.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MapsApp />
   </React.StrictMode>,
 )
