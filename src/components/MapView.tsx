29624cf4155ede97022e6b1a5177eb510c223c52
Hugo Ballesteros <hugo@boahstudio.com>
2023-08-21 22:44:30 -0300

    Mapbox setup

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,17 +1,39 @@
-import { useContext } from 'react'
+import { useContext, useRef, useLayoutEffect } from 'react'
+import { Map } from 'mapbox-gl'
+
 import { PlacesContext } from '../context'
 import { Loading } from '.'
 
 export const MapView = () => {
 
     const { isLoading, userLocation } = useContext( PlacesContext )
+    const mapDiv = useRef<HTMLDivElement>( null )
+
+    useLayoutEffect(() => {
+        if ( !isLoading ) {
+            const map = new Map({
+                container: mapDiv.current!, // container ID
+                style: 'mapbox://styles/mapbox/streets-v12', // style URL
+                center: userLocation, // starting position [lng, lat]
+                zoom: 14, // starting zoom
+            }); 
+        }
+    }, [ isLoading ])
 
     if ( isLoading ) {
         return ( <Loading /> )
     }
 
     return (
-        <div>
+        <div ref={ mapDiv }
+            style={{
+                height: '100vh',
+                left: 0,
+                position: 'fixed',
+                top: 0,
+                width: '100vw',
+            }}
+        >
             { userLocation?.join(', ') }
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import { MapsApp } from './MapsApp.tsx'
 
+import mapboxgl from 'mapbox-gl'// or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaHVndWljaHUiLCJhIjoiY2xmbnhuN25nMHFkdzNzbXZxbDV4anRkbCJ9.M88ulvz04sFZXRY-sLruzw';
+
 
 if ( !navigator.geolocation ) {
   alert('Geolocation is not supported by your browser')
